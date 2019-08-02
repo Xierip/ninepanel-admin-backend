55fb78d62e79fbe88b1c1c690b71cb7e5ccaae32
Patryk Bernacki <patrykbernacki@outlook.com>
2019-08-02 14:13:38 +0200

    Make method for duplicated code

--- src/main/java/dev/nine/ninepanel/service/domain/ServiceRepository.java
+++ src/main/java/dev/nine/ninepanel/service/domain/ServiceRepository.java
@@ -24,16 +24,18 @@ interface ServiceRepository extends MongoRepository<Service, ObjectId> {
   Page<Service> findAllByClientId(ObjectId clientId, Pageable pageable);
 
   default void deleteByIdOrThrow(ObjectId serviceId) {
-    if (!this.existsById(serviceId)) {
-      throw new ServiceNotFoundException(serviceId);
-    }
+    checkIfExistsOrThrow(serviceId);
     deleteById(serviceId);
   }
 
   default Service updateOrThrow(Service service) {
-    if (!this.existsById(service.getId())) {
-      throw new ServiceNotFoundException(service.getId());
-    }
+    checkIfExistsOrThrow(service.getId());
     return save(service);
+  }
+
+  private void checkIfExistsOrThrow(ObjectId serviceId) {
+    if (!this.existsById(serviceId)) {
+      throw new ServiceNotFoundException(serviceId);
+    }
   }
 }
