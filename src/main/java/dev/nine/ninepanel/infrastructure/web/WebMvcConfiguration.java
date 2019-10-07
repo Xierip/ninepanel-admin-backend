7e5ec54fb882a32d6e8a35cdd8ebf7077803f544
Patryk Bernacki <patrykbernacki@outlook.com>
2019-10-07 23:48:09 +0200

    Logs

--- src/main/java/dev/nine/ninepanel/infrastructure/web/WebMvcConfiguration.java
+++ src/main/java/dev/nine/ninepanel/infrastructure/web/WebMvcConfiguration.java
@@ -1,7 +1,9 @@
 package dev.nine.ninepanel.infrastructure.web;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.filter.CommonsRequestLoggingFilter;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -20,6 +22,16 @@ class WebMvcConfiguration implements WebMvcConfigurer {
         .allowedHeaders("Authorization", "Cache-Control", "Content-Type")
         .exposedHeaders("Location")
         .maxAge(3600);
+  }
+
+  @Bean
+  public CommonsRequestLoggingFilter requestLoggingFilter() {
+    CommonsRequestLoggingFilter loggingFilter = new CommonsRequestLoggingFilter();
+    loggingFilter.setIncludeClientInfo(true);
+    loggingFilter.setIncludeQueryString(true);
+    loggingFilter.setIncludePayload(true);
+    loggingFilter.setIncludeHeaders(true);
+    return loggingFilter;
   }
 
 }
